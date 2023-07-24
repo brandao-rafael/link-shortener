56365f512bda3eb72959a3dceb29dbc8ce918fe4
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-07-24 18:56:43 -0300

    feat: add cors middleware with default options

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import shortenerRouter from './routes/shortener.routes';
 import errorMiddleware from './middlewares/errorMiddleware';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use('/', shortenerRouter);
 
