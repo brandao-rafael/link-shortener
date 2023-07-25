bf3a6d354ad361b14392108c31cf0251f823b4e7
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-07-25 17:36:44 -0300

    refactor: change cors middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,18 +7,26 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ['https://linkshortener.up.railway.app/'];
-const corsOptions: cors.CorsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin!) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://linkshortener.up.railway.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-app.use(cors(corsOptions));
+// const allowedOrigins = ['https://linkshortener.up.railway.app/'];
+// const corsOptions: cors.CorsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin!) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+
+
+// app.use(cors(corsOptions));
 
 app.use('/', shortenerRouter);
 
