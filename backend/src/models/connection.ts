8e9f5dbba4df98558711c344e70187fb530f669c
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-07-25 16:35:57 -0300

    refactor: add try/catch to connection

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
-import 'dotenv/config';
 
-const connectToDatabase = (mongoUri = process.env.MONGO_URI) => mongoose.connect(mongoUri!);
+const connectToDatabase = async () => {
+  const mongoUri = `${process.env.MONGO_URL}/linkShortener`;
+  try {
+    await mongoose.connect(mongoUri!);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
 
 export default connectToDatabase;
