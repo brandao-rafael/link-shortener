2191d8fc2334cde45443e58847007cabb1edea1c
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-07-25 18:41:16 -0300

    refactor: add error if env variable are undefined

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 const connectToDatabase = async () => {
   const mongoUri = `${process.env.MONGO_URL}/linkShortener`;
   try {
+    if(mongoUri.split('/').includes('undefined')) {
+      throw new Error('Env variables not read');
+    }
     await mongoose.connect(mongoUri!);
     console.log("Connected to MongoDB");
   } catch (error) {
