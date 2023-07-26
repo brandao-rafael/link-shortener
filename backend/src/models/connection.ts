8d4f0b13823d1ca2bcc12005519f810da6a56428
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-07-26 13:40:56 -0300

    refactor: change endpoint of db

--- backend/src/models/connection.ts
+++ backend/src/models/connection.ts
@@ -2,10 +2,7 @@ import 'dotenv/config';
 import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
-  const mongoUri = `${process.env.MONGO_URL}/linkShortener`;
-  console.log('MONGO_URL:', process.env.MONGO_URL);
-  console.log('MONGOUSER:', process.env.MONGOUSER);
-  console.log('MONGOPASSWORD:', process.env.MONGOPASSWORD);
+  const mongoUri = `${process.env.MONGO_URL}`;
   try {
     await mongoose.connect(mongoUri!);
     console.log("Connected to MongoDB");
